30d195623e65a7fab27c8cbaa69e862b6d749aa6
Tim S. <tim.suellner@web.de>
2025-05-19 02:56:43 +0200

    fix: api url

--- app/src/context/AuthContext.tsx
+++ app/src/context/AuthContext.tsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useState, useEffect } from 'react'
 import type { ReactNode } from 'react'
 
-const API_URL = (import.meta.env.VITE_API_URL || 'http://localhost:8080') + "/api/v1"
+const API_URL = (import.meta.env.VITE_API_URL || '') + "/api/v1"
 
 interface AuthContextType {
   isAuthenticated: boolean
